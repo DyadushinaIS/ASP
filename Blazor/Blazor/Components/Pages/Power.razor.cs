e36d57175af31abfb298f98e814f9f94233f3781
DyadushkinaIS <irinadyadushkina@gmail.com>
2026-09-09 14:48:23 +0500

    Homework power DONE

---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		// Получаем число
+		int number = 1;
+		// Получаем степень
+		int exponent = 1;
+		BigInteger result = 1;
+		void Power_result()
+		{
+			// Возводим в степень через умножение
+			result = 1;
+			for (int i = 1; i <= exponent; i++)
+			{ result *= number; }
+		}
+	}			
+}
+				
+	
