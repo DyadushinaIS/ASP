52c4f3e5fd930ed3db6fc30c39b454266e1b66f9
DyadushkinaIS <irinadyadushkina@gmail.com>
2026-09-11 16:54:34 +0500

    homework1 almost done

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace Blazor.Components.Pages
+{
+	public partial class Fibonacci
+	{
+		int amount = 0;
+		string result = "";
+
+		void Fibonacci_row()
+		{
+			int n1 = 1;
+			int n2 = 1;
+			
+			if (amount == 1)
+				result += "F1=1 ";
+			if (amount == 2)
+				result += "F2=1 ";
+			if (amount>2)
+			{
+				for (int i = 3;  i < amount; i++)
+				{
+					int n3 = n1 + n2;
+					result = result + "F" + i + "=" + n3+"\n "; 
+					n1=n2;
+					n2=n3;
+				}
+			}
+		}
+	}
+}
