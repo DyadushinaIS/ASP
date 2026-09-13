17a8643e74ca7c89730663cedb86d7f8c1e6ed8e
irina <irina@COMPUTER>
2026-09-13 10:51:03 +0500

    Lesson 20 DataAnnotations

--- Blazor/Movies/Models/Movie.cs
+++ Blazor/Movies/Models/Movie.cs
@@ -1,10 +1,20 @@
-﻿namespace Movies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        //[StringLength(50,MinimumLength=2)]
+        [MinLength(2,ErrorMessage ="Слишком мало"), MaxLength(50,ErrorMessage ="Слишком много")]
+        [Display(Name ="Название")]
         public string Title { get; set; }
+        [RangeAttribute(typeof(DateOnly),"1888-10-14","2128-12-31",ErrorMessage ="Тогда кино либо еще не снимали, или уже ничего не снимут")]
+        [Display(Name ="Дата выхода")]
         public DateOnly ReleeaseDate {  get; set; }
+        //[DisplayName ="Жанр"]
         public string ? Genre { get; set; }
         public string ? URL { get; set; }
         public string Poster { get; set; }
