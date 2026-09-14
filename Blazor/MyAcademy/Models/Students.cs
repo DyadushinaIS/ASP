de6e217b0fd6db423c834c5b8e48b96b9652692e
irina <irina@COMPUTER>
2026-09-14 07:15:29 +0500

    Start MyAcademy

---
+++ Blazor/MyAcademy/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿namespace MyAcademy.Models
+{
+    public class Students
+    {
+        public int stud_id { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string? middle_name { get; set; }
+        public string birth_date { get; set; }
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public string? photo { get; set; }
+        public int group_number { get; set; }
+    }
+}
