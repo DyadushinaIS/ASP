d2a15d144b9f545ed53153b1fa86ae078d2a312b
irina <irina@COMPUTER>
2026-09-18 17:13:40 +0500

    Power&Fibonacci Homework done

---
+++ Blazor/Academy/Models/Group.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+    public class Group
+    {
+        [Key]
+        [Column (TypeName ="TINYINT")]
+        public int group_id {  get; set; }
+        [Required]
+        [StringLength(10,MinimumLength =4)]
+        public string group_name { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Direction))]
+        public int direction { get; set; }
+        public DateOnly? start_date { get; set; }
+        public TimeOnly? start_time { get; set; }
+        [Column (TypeName ="TINYINT")]
+        public int? learning_days { get; set; }
+
+        //Navigation properties
+        public Direction Direction { get; set; }
+    }
+}
--- Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -1,29 +1,39 @@
-﻿namespace Blazor.Components.Pages
-{
-	public partial class Fibonacci
-	{
-		int amount = 0;
-		string result = "";
+﻿using System.Numerics;
 
-		void Fibonacci_row()
-		{
-			int n1 = 1;
-			int n2 = 1;
-			
-			if (amount == 1)
-				result += "F1=1 ";
-			if (amount == 2)
-				result += "F2=1 ";
-			if (amount>2)
-			{
-				for (int i = 3;  i < amount; i++)
-				{
-					int n3 = n1 + n2;
-					result = result + "F" + i + "=" + n3+"\n "; 
-					n1=n2;
-					n2=n3;
-				}
-			}
-		}
-	}
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        int amount = 0;
+        string result = "";
+
+        void Fibonacci_row()
+        {
+            BigInteger n1 = 1;
+            BigInteger n2 = 1;
+
+            if (amount == 1)
+            {
+                result = null;
+                result += "F1=1 ";
+            }
+            if (amount == 2)
+            {
+                result = null;
+                result += "F1=1 F2=1 ";
+            }
+            if (amount > 2)
+            {
+                result = null;
+                result += "F1=1 F2=1 ";
+                for (int i = 2; i < amount; i++)
+                {
+                    BigInteger n3 = n1 + n2;
+                    result = result + "F" + (i+1) + "=" + n3 + "\n ";
+                    n1 = n2;
+                    n2 = n3;
+                }
+            }
+        }
+    }
 }
